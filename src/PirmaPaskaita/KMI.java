ef2c898733064d828ebee718810685a447e944ce
Mindaugas <m.ciudaras@gmail.com>
2017-04-05 21:58:43 +0300

    commit

--- src/PirmaPaskaita/KMI.java
+++ src/PirmaPaskaita/KMI.java
@@ -4,7 +4,15 @@ package PirmaPaskaita;
  * Created by Mindaugas on 2017-04-05.
  */
 public class KMI {
-    public static float KMI(float weight, float height){
+    private float weight;
+    private float height;
+
+    public KMI(float weight, float height){
+        this.weight = weight;
+        this.height = height;
+    }
+
+    public float KMI(){
         return weight / (height * height);
     }
 }
--- src/PirmaPaskaita/Uzd_1_12.java
+++ src/PirmaPaskaita/Uzd_1_12.java
@@ -6,15 +6,20 @@ import java.util.Scanner;
  * Created by Mindaugas on 2017-04-05.
  */
 public class Uzd_1_12 {
-    public static void main(String[] args){
-        KMI kmi = new KMI();
+    private static KMI kmi;
 
+    public static void main(String[] args) {
+        Uzd_1_12();
+        System.out.println(kmi.KMI());
     }
-    public Uzd_1_12(float weight, float height) {
+
+    public static void Uzd_1_12() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Iveskite savo svori");
-        weight = scanner.nextFloat();
+        float weight = scanner.nextFloat();
         System.out.println("Iveskite savo ugi");
-        height = scanner.nextFloat();
+        float height = scanner.nextFloat();
+        kmi = new KMI(weight, height);
+
     }
 }
